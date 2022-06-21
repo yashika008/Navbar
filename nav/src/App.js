2e24789c66f2a0fd70153acecce65fa50a545d89
yashika008 <93548785+yashika008@users.noreply.github.com>
2022-06-22 02:48:43 +0530

    Add files via upload

---
+++ nav/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar>
+
+     </Navbar>
+     
+
+    </div>
+  );
+}
+
+export default App;
